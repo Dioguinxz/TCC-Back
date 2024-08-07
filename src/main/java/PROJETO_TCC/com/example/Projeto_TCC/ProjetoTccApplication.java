cccc442cab25045dee464b75d27fc6721fd216f0
Dioguinxz <doassis@hotmail.com.br>
2024-08-07 20:19:00 -0300

    initial commit

---
+++ src/main/java/PROJETO_TCC/com/example/Projeto_TCC/ProjetoTccApplication.java
@@ -0,0 +1,13 @@
+package PROJETO_TCC.com.example.Projeto_TCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PROJETO_TCC/com/example/Projeto_TCC/ProjetoTccApplicationTests.java
@@ -0,0 +1,13 @@
+package PROJETO_TCC.com.example.Projeto_TCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
