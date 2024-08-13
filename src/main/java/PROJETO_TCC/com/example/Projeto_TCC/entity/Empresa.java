84c5bfe729a362baa3ab213cb6056c9dbb1e68d5
Dioguinxz <doassis@hotmail.com.br>
2024-08-13 15:37:12 -0300

    Criação da classe entidade Empresa

---
+++ src/main/java/PROJETO_TCC/com/example/Projeto_TCC/entity/Empresa.java
@@ -0,0 +1,69 @@
+package PROJETO_TCC.com.example.Projeto_TCC.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "Nome", length = 200, nullable = false)
+    private String nome;
+
+    @Column(name = "Email", nullable = false, unique = true)
+    private String email;
+
+
+    @Column(name = "Senha", nullable = false)
+    private String senha;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "empresa_role", joinColumns = @JoinColumn(name = "empresa_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+}
