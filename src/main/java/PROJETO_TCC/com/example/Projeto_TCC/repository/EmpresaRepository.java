ff4d74fce5b49392877592c21dabfd347bb8f807
Dioguinxz <doassis@hotmail.com.br>
2024-08-13 15:41:08 -0300

    Criação da classe EmpresaRepository

---
+++ src/main/java/PROJETO_TCC/com/example/Projeto_TCC/repository/EmpresaRepository.java
@@ -0,0 +1,7 @@
+package PROJETO_TCC.com.example.Projeto_TCC.repository;
+
+import PROJETO_TCC.com.example.Projeto_TCC.entity.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+}
